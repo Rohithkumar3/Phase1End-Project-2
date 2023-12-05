48884c62bb5aca0a074b471910c1aa7a139edcc6
Rohithkumar <rohithkumargande@gmail.com>
2023-12-05 19:23:56 +0530

    PhaseEndProject2

---
+++ Phase1Project2/Teacher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Project2
+{
+    public class Teacher
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int ClassNum { get; set; }
+        public string Section { get; set; }
+    }
+}
